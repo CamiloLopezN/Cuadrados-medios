449d8652f6c87334f8875518f90c9f03145df8df
cristian.lopez09825@gmail.com <cristian.lopez09825@gmail.com>
2022-05-10 22:29:21 -0500

    Se agregaron los casos de prueba

--- src/run/Runner.java
+++ src/run/Runner.java
@@ -4,18 +4,24 @@ import controller.MidSquare;
 
 public class Runner {
 
+    /*
+     * Si se desea hacer uso de un caso de prueba en particular basta con comentar el caso de prueba
+     * que se está evaluando con el fin de que no entorpezca la lectura de los datos del caso de prueba
+     * que se desea probar, posteriormente llame el caso de prueba que desea evaluar pasando como tercer
+     * parametro la cantidad de numeros que desea generar.
+     * */
+
     public static void main(String[] args) {
-        int digits = 4;
-        int seed = 727;
-        int count = 100;
-
-        long timeStart = System.currentTimeMillis();
-        new MidSquare(digits, seed, count).generateValues();
-        long timeEnd = System.currentTimeMillis();
-
-        System.out.println("Tiempo de: " + (timeEnd - timeStart));
-
+        //  caseOne(4, 727, 100);
+        caseTwo(4, 128, 100);
     }
 
+    public static void caseOne(int digits, int seed, int count) {
+        new MidSquare(digits, seed, count).generateValues();
+    }
+
+    public static void caseTwo(int digits, int seed, int count) {
+        new MidSquare(digits, seed, count).generateValues();
+    }
 
 }
